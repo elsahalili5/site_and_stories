8d514bbb24c4052f2b9d841e9e25051aed383516
elsahalili5 <126355555+elsahalili5@users.noreply.github.com>
2023-09-20 21:53:11 +0200

    add new page and fix performance

---
+++ js/case.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var arrowBtn = document.getElementById("arrow-icon");
+  arrowBtn.onclick = () => {
+    window.location.href = "index.html";
+  };
+});
--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-function openMenu() {
-  var menu = document.getElementById("myLinks");
-  if (menu.style.display === "block") {
-    menu.style.display = "none";
-  } else {
-    menu.style.display = "block";
-  }
-}
+// function openMenu() {
+//   var menu = document.getElementById("myLinks");
+//   if (menu.style.display === "block") {
+//     menu.style.display = "none";
+//   } else {
+//     menu.style.display = "block";
+//   }
+// }
 window.addEventListener("load", function () {
   var title = document.getElementById("title-wrapper");
   var pageContent = document.getElementById("page-content");
