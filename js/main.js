eaad8c1c544d8df6f4b7b089ec962fc0e293ccef
elsahalili5 <126355555+elsahalili5@users.noreply.github.com>
2023-09-20 14:40:02 +0200

    add new feature

--- js/main.js
+++ js/main.js
@@ -6,3 +6,16 @@ function openMenu() {
     menu.style.display = "block";
   }
 }
+window.addEventListener("load", function () {
+  var title = document.getElementById("title-wrapper");
+  var pageContent = document.getElementById("page-content");
+
+  setTimeout(function () {
+    if (title) {
+      title.style.display = "none";
+    }
+    if (pageContent) {
+      pageContent.style.display = "block";
+    }
+  }, 1500);
+});
