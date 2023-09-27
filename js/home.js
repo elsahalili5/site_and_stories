7ab60f09d8c28a0115d04461fd57c3abf3544f99
elsahalili5 <126355555+elsahalili5@users.noreply.github.com>
2023-09-27 20:17:48 +0200

    fix homepage errors and enhance website responsiveness

--- js/home.js
+++ js/home.js
@@ -21,7 +21,20 @@ cities.forEach((city, index) => {
   city.addEventListener("click", () => {
     const casesWrappers = document.querySelectorAll(".cases-wrapper");
     const casesWrapper = casesWrappers[index];
+    const prevCase = document.querySelector(".show-cases");
+    const clickedCase = document.querySelector(".cases-wrapper.show-cases");
 
+    if (prevCase) {
+      prevCase.classList.remove("show-cases");
+    }
+
+    // casesWrappers.forEach((element) => {
+    //   element.classList.remove("show-cases");
+    // });
     casesWrapper.classList.toggle("show-cases");
+
+    if (clickedCase) {
+      clickedCase.classList.remove("show-cases");
+    }
   });
 });
